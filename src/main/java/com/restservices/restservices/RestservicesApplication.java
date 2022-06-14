ef9aef2d7b8939f4c8c3b0443f31d17ed00a1788
amritj <amritansh7757@gmail.com>
2022-06-14 11:01:36 +0530

    "Second Commit"

--- src/main/java/com/restservices/restservices/RestservicesApplication.java
+++ src/main/java/com/restservices/restservices/RestservicesApplication.java
@@ -2,7 +2,7 @@ package com.restservices.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//new setup
 @SpringBootApplication
 public class RestservicesApplication {
 
